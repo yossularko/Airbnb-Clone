97655ea5bef67e1bd1a4d5a8d1e2395657e27bb9
radenlarko <radenlarko@gmail.com>
2022-06-03 10:00:24 +0700

    Update library react-map-gl

--- components/Maps.js
+++ components/Maps.js
@@ -10,8 +10,6 @@ const Maps = ({ searchResult }) => {
   const { latitude, longitude } = getCenter(coordinates);
 
   const [viewport, setViewport] = useState({
-    width: "100%",
-    height: "100%",
     latitude,
     longitude,
     zoom: 11,
@@ -21,8 +19,9 @@ const Maps = ({ searchResult }) => {
     <ReactMapGL
       mapStyle="mapbox://styles/mapbox/dark-v9"
       mapboxAccessToken={process.env.mapbox_key}
-      {...viewport}
-      onMove={(e) => setViewport(e.viewState)}
+      initialViewState={viewport}
+      style={{ width: "100%", height: "100%" }}
+      onMove={(e) => console.log('viewport: ', e)}
     >
       {searchResult.map(({ lat, long }) => (
         <Marker key={lat} latitude={lat} longitude={long} anchor="bottom">
